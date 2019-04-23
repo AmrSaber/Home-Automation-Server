f44e931c3d22b1796acbcfcced8565cd441fc94b
Amr Saber <amr.m.saber.mail@gmail.com>
2019-04-23 17:32:48 +0200

    Fix error in GET /temperature

--- src/routers/temperature.js
+++ src/routers/temperature.js
@@ -4,7 +4,7 @@ const db = require('../db')
 const router = new express.Router()
 
 //get temperature 
-router.get('/temperature', (req, res) =>{
+router.get('/', (req, res) =>{
 	res.send({ temperature: db.getTemperature()})
 })
 
